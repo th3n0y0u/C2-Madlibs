7d500875aced316e46c9a19748f1b80518f630af
soop <110252@eesd.org>
2021-03-25 23:37:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Welcome to a totally not a Madlibs where I didn't get forced into this for homework")
+
+unfunnyword = str(input("Give me a really funny word that is really unfunny "))
+verb = str(input("Great now give me a verb "))
+adjective = str(input("Now give me a adjective"))
+positive = str(input("Now give me a actually positive word"))
+longword = str(input("Now put a long word in here"))
+
+print("starting to boot up the discount madlibs...")
+print("I tried to make a joke including " + unfunnyword + "and I " + verb + "from the scary monster that came out of a person because I said a really unfunny joke. " + "The monster was " + adjective + "I ran away as fast as I can and I met a tree that was " + positive + "now the tree spoke " + longword + "and I instantly died due to complications of dealing with the " + longword + ".") 
